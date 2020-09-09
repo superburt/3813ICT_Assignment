99e6afc05d0c3f123c770dd127ef4aca6c411784
superburt <53563328+superburt@users.noreply.github.com>
2020-09-09 21:44:54 +1000

    added server.js code for body parser and for get send file to enable reading of a json with data

--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const sockets = require('./socket.js');
 const server = require('./listen.js');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
+app.use(bodyParser.json());
 app.use(cors());
 
 sockets.connect(io, PORT)
 
 server.listen(http, PORT);
 //this is just a comment to test my new branch 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/../dist/index');
+});
