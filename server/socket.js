9a1505fb209f38d9bbd7bf5c41a2298cf4194e89
superburt <53563328+superburt@users.noreply.github.com>
2020-09-09 21:49:04 +1000

    added fs constant to utilize fs.readFile and fs.writeFile functions

--- server/socket.js
+++ server/socket.js
@@ -1,3 +1,9 @@
+// https://nodejs.org/api/fs.html -> lot of good information here on reading and writing with node file system (fs)
+const fs = require('fs');
+const file = require('../src/assets/data.json')
+
+//TO BE CONTINUED -> NEXT TO IMPLEMENT fs.readFile in module expoerts 
+
 module.exports = {
     connect: function(io, PORT){
         io.on('connection',(socket)=> {
